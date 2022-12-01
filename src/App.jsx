ab51ee2282a2f8f7465f75ce9ae226aa9ea4b32e
Mykola Rudenko <m.rudenko.rb@gmail.com>
2022-12-01 19:44:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { ModalContent } from './components/ModalContent/index';
+//
+//
+//
+//
+//
+//
+//
+function App() {
+  return (
+    <div className="App">
+      <ModalContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ModalAnimated/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+//
+import styles from "./ModalAnimated.module.scss";
+//
+//
+//
+//
+//
+//
+//
+export const ModuleAnimated = ({opened, onClose, children}) => {
+if (!opened) {
+	return null;
+}
+
+  return <div>
+	<Portal>
+		
+	</Portal>
+  </div>;
+};
